aada805e107b6b3cf0872b161c69ceab315f2c69
TheDrBee <drbee@gmx.net>
2021-10-01 12:10:53 +0200

    cosmetic: rename transition and improve comments

--- js/Ownership.js
+++ js/Ownership.js
@@ -7,14 +7,15 @@ const {
 
 async function run()
 {
-  async function call_foo()
+  async function call_IsOwnerCalling()
   {
-    const tx = await sc_call(sc, "Foo");
-    console.log(`event_logs[0].params[2].value after call to Foo() from account 0:\n`,
+    const tx = await sc_call(sc, "IsOwnerCalling");
+    console.log(`event_logs[0].params[2].value after call to IsOwnerCalling() from account 0:\n`,
       tx.receipt.event_logs[0].params[2]);
   }
 
   let tx = sc = state = null;
+
   try { // deploy the contract with account[0] the initial owner
     const init = [
       { vname: '_scilla_version',     type: 'Uint32',   value: '0',},
@@ -25,30 +26,26 @@ async function run()
     state = await sc.getState();
     console.log(`state after deployment from ${setup.addresses[0]}:\n`, state);
 
-    try { // call foo(.) and log emitted events params indicating if caller was the owner
-      await call_foo();
+    try { // call IsOwnerCalling(.) and log emitted events params indicating if caller was the owner
+      await call_IsOwnerCalling();
       try { // call change_owner to change owner to 2nd address and log state
-        const new_owner = setup.addresses[1];
-        const args = [ { vname: 'new_owner', type: 'ByStr20',  value: new_owner },];
+        let new_owner = setup.addresses[1];
+        let args = [ { vname: 'new_owner', type: 'ByStr20',  value: new_owner },];
         tx = await sc_call(sc, "ChangeOwner", args);
         state = await sc.getState();
         console.log(`event_logs[0].params[2] after call to ChangeOwner to ${new_owner}:\n`,
           tx.receipt.event_logs[0].params[2]);
         console.log(`state after call to ChangeOwner(${new_owner}):\n`, state);
-        try { // call foo(.) and log emitted events params indicating if caller was the owner
-          await call_foo();
-        } catch (err) {
-          console.log("Foo(): ERROR\n",err);
+        try { // call IsOwnerCalling(.) and log emitted events params indicating if caller was the owner
+          await call_IsOwnerCalling();
         }
-      } catch (err) {
-        console.log("ChangeOwner(.): ERROR\n",err)
+        catch (err) { console.log("IsOwnerCalling(): ERROR\n",err); }
       }
-    } catch (err) {
-      console.log("Foo(): ERROR\n",err);
+      catch (err) { console.log("ChangeOwner(.): ERROR\n",err); }
     }
-  } catch (err) {
-    console.log("deploy_from_file(.): ERROR\n",err);
+    catch (err) { console.log("IsOwnerCalling(): ERROR\n",err); }
   }
+  catch (err) { console.log("deploy_from_file(.): ERROR\n",err); }
 }
 
 run();
